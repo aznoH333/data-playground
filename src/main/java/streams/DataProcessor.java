83df490a1bc38f131a945f2e241ddacf8475f086
aznoH333 <glentik23@gmail.com>
2021-12-02 20:48:21 +0100

    g

--- src/main/java/streams/DataProcessor.java
+++ src/main/java/streams/DataProcessor.java
@@ -1,55 +1,90 @@
 package streams;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class DataProcessor {
 
 
+    private static Function<Grade, GradeType> getType;
+
     public static boolean atLeastOneGradeA(Student student) {
-        return false;
+        return student.getGrades().stream().anyMatch(grade -> {
+            return grade.getType() == GradeType.A;
+        });
     }
 
 
     public static List<Integer> getStudentAges(List<Student> students) {
-        return new ArrayList<>();
+        return students.stream().map(i -> i.getAge()).collect(Collectors.toList());
     }
 
     public static List<Student> getStudentsWithMinimumAge(List<Student> students, int minAge) {
-        return new ArrayList<>();
+        return students.stream().filter(s -> {
+            return s.getAge() >= minAge;
+        }).collect(Collectors.toList());
     }
 
 
     // getGender() == Gender.MALE
     // or getGender().name().equals("MALE")
     public static long countMaleStudents(List<Student> students) {
-        return 0;
+        return students.stream().filter(s -> {
+            return s.getGender() == Gender.MALE;
+        }).count();
     }
 
-
     public static double avgAgeOfFemaleStudent(List<Student> students) {
-        return 0.0;
+        return students.stream().filter(s -> {
+            return s.getGender() == Gender.FEMALE;
+        }).mapToDouble(s -> {
+            return s.getAge();
+        }).average().getAsDouble();
     }
 
     public static Integer getProductOfStudentAges(List<Student> students) {
-        return 0;
+        return students.stream().mapToInt(s -> {
+            return s.getAge();
+        }).reduce(1, (acc, i) -> {
+            return acc * i;
+        });
     }
 
     // ignore F Grades
     public static double productOfStudentGrades(Student student) {
-        return 0.0;
+        return student.getGrades()
+                .stream()
+                .mapToInt(i -> {
+                    return i.getType().getValue();
+                })
+                .reduce(1, (acc, i) -> {
+                    if (i == 0) return acc;
+                    return i * acc;
+                });
     }
 
     // region BONUS
 
     public static Optional<Grade> getBestMathGradeFromStudent(Student student) {
-        return Optional.empty();
+        return Optional.of(student.getGrades()
+                .stream()
+                .filter(i -> {
+                    return i.getSubject() == Subject.MATH;
+                })
+                .reduce(new Grade(Subject.MATH,GradeType.F),(grade, i) -> {
+                    if (grade.getType().getValue() > i.getType().getValue()) return grade;
+                    return i;
+                }));
     }
 
     public static List<Integer> getSortedAges(List<Student> students) {
-        return new ArrayList<>();
+
+        return students.stream().map(s -> s.getAge()).sorted(Comparator.naturalOrder()).collect(Collectors.toList());
     }
 
-    // endregion
+
+// endregion
 }
