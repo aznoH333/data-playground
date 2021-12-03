55ed19a102d13c986938996448441a3145a01ec4
aznoH333 <glentik23@gmail.com>
2021-12-03 09:35:46 +0100

    yaba daba doo!

--- src/main/kotlin/processing/data_processing.kt
+++ src/main/kotlin/processing/data_processing.kt
@@ -4,50 +4,50 @@ import streams.*
 
 
 fun atLeastOneGradeA(student: Student): Boolean {
-    TODO()
+    return student.grades.any { i -> i.type == GradeType.A }
 }
 
 
 fun getStudentAges(students: List<Student>): List<Int> {
-    TODO()
+    return students.map { i -> i.age}
 }
 
 fun getStudentsWithMinimumAge(students: List<Student>, minAge: Int): List<Student> {
-    TODO()
+    return students.filter { i -> i.age >= minAge }
 }
 
 
 // gender == Gender.MALE
 // or gender.name == "MALE"
 fun countMaleStudents(students: List<Student>): Int {
-    TODO()
+    return students.count { i -> i.gender == Gender.MALE }
 }
 
 
 // gender == Gender.FEMALE
 // or gender.name == "FEMALE"
 fun avgAgeOfFemaleStudent(students: List<Student>): Double {
-    TODO()
+    return students.filter { i -> i.gender == Gender.FEMALE }.map { i -> i.age }.average()
 }
 
 fun getProductOfStudentAges(students: List<Student>): Int {
-    TODO()
+    return students.map { i -> i.age }.reduce { acc,i -> acc*i }
 }
 
 // ignore F Grades
 fun productOfStudentGrades(student: Student): Int {
-    TODO()
+    return student.grades.map { i -> i.type.value }.filter { i -> i != 0 }.reduce {acc,i -> acc*i}
 }
 
 // region BONUS
 
 // use maxByOrNull on grades
 fun getBestMathGradeFromStudent(student: Student): Grade? {
-    TODO()
+    return student.grades.filter { i -> i.subject == Subject.MATH }.maxByOrNull { i -> i.type.value }
 }
 
 fun getSortedAges(students: List<Student>): List<Int> {
-    TODO()
+    return students.map { i -> i.age }.sorted()
 }
 
 // endregion
